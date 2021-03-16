9cf4bbc91464b634945d516cb866decb88bcadea
Victor Jose Guichardo Concepcion <victor_guichardo@ucne.edu.do>
2021-03-16 18:57:18 -0400

    Create JugadorBLL.cs
    BLL

---
+++ SimuladorBateo/BLL/JugadorBLL.cs
@@ -0,0 +1,40 @@
+﻿using SimuladorBateo.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimuladorBateo.BLL
+{
+    public class JugadorBLL
+    {
+        public static Jugadores Buscar(int id)
+        {
+            Jugadores a = new Jugadores();
+            try
+            {
+                foreach (var item in Simulador.ListaJugadores)
+                {
+                    if (item.JugadorId == id)
+                    {
+                        a = item;
+                    }
+                }
+                return a;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public static List<Jugadores> GetJugadores()
+        {
+            List<Jugadores> l = new List<Jugadores>();
+
+            l = Simulador.ListaJugadores;
+
+            return l;
+        }
+    }
+}
